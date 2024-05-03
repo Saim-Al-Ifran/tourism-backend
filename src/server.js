de79d9794bba2e2c724f022dce84726924a9ff25
Saim <admin@gmail.com>
2024-05-03 23:24:08 +0600

    added cors and essential middlewares

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-
 const express = require('express');
 const connectDB = require('./config/db');
- 
+const cors = require('cors'); 
 const app = express();
 
+app.use(express.json())
+app.use(cors());
  
 app.get('/', (req, res) => {
     res.status(200).json({message:'Hello, Express!'});
