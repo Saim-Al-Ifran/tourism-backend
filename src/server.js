62e947b97ca98c5e389a9070dfa20a09e6ba7e20
Saim <admin@gmail.com>
2024-05-03 20:16:36 +0600

    testing dummy routes

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 
  
-// app.get('/', (req, res) => {
-//     res.send('Hello, Express!');
-// });
+app.get('/', (req, res) => {
+    res.status(200).json({message:'Hello, Express!'});
+});
  
 const port = 3000; 
 app.listen(port, () => {
